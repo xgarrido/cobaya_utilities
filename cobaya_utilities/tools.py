a240164e5adad03aa19a3269c7703b9e5fc7180b
Xavier Garrido <xavier.garrido@gmail.com>
2020-11-03 23:16:07 +0100

    add gelman R-1 plots

--- cobaya_utilities/tools.py
+++ cobaya_utilities/tools.py
@@ -2,6 +2,7 @@ import glob
 import os
 
 import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def print_chains_size(mcmc_dir, mcmc_names=None, tablefmt="html"):
@@ -11,7 +12,7 @@ def print_chains_size(mcmc_dir, mcmc_names=None, tablefmt="html"):
     ----------
     mcmc_dir: str or list
       a base directory holding the MCMC samples. Either a string holding regex expression
-      of a list of directories
+      or a list of directories
     mcmc_names: list
       a list of names which corresponds to the directory names.
     tablefmt: str
@@ -82,3 +83,42 @@ def plot_chains(mcmc_dir, params, title=None, nrow=None, ncol=None):
             ax[i].set_ylabel(r"${}$".format(lookup[p].get("label")))
             ax[i].plot(sample.samples[:, lookup[p].get("pos")], alpha=0.75, color=color)
     plt.tight_layout()
+
+
+def plot_progress(mcmc_dir, mcmc_names=None):
+    """Plot Gelman R-1 parameter and acceptance rate
+
+    Parameters
+    ----------
+    mcmc_dir: str or list
+      a base directory holding the MCMC samples. Either a string holding regex expression
+      or a list of directories
+    mcmc_names: list
+      a list of names which corresponds to the directory names.
+    """
+
+    if isinstance(mcmc_dir, str):
+        mcmc_dir = glob.glob(mcmc_dir)
+
+    if mcmc_names is None:
+        mcmc_names = [os.path.basename(d) for d in mcmc_dir]
+
+    nrow = (len(mcmc_dir) + 1) // 2
+    ncol = (len(mcmc_dir) + 1) // 2
+    fig, ax = plt.subplots(2 * nrow, ncol, figsize=(15, 10), sharex=True)
+
+    for i, d in enumerate(mcmc_dir):
+        files = sorted(glob.glob(os.path.join(d, "mcmc.?.progress")))
+        for f in files:
+            cols = [a.strip() for a in open(f).readline().lstrip("#").split()]
+            df = pd.read_csv(
+                f, names=cols, comment="#", sep=" ", skipinitialspace=True, index_col=False
+            )
+            idx = f.split(".")[-2]
+            kwargs = dict(label="mcmc #{}".format(idx), color="C{}".format(idx), alpha=0.75)
+            ax[i, 0].semilogy(df.N, df.Rminus1, "-o", **kwargs)
+            ax[i, 0].set_ylabel(r"$R-1$")
+            ax[i, 1].plot(df.N, df.acceptance_rate, "-o", **kwargs)
+            ax[i, 1].set_ylabel(r"acceptance rate")
+        leg = ax[i, 1].legend(title=mcmc_names[i], bbox_to_anchor=(1, 1), loc="upper left")
+        leg._legend_box.align = "left"
