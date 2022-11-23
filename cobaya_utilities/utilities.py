f1f250189164549199b2f52d76d98eb4ff9fa21d
Xavier Garrido <xavier.garrido@gmail.com>
2022-11-23 21:18:51 +0100

    add utilities for misc operation or global variable

---
+++ cobaya_utilities/utilities.py
@@ -0,0 +1,10 @@
+_cosmo_labels = dict(
+    cosmomc_theta=r"\theta_\mathrm{MC}",
+    logA=r"\log(10^{10} A_\mathrm{s})",
+    As=r"A_\mathrm{s}",
+    Alens=r"A_\mathrm{lens}",
+    ns=r"n_\mathrm{s}",
+    ombh2=r"\Omega_\mathrm{b}h^2",
+    omch2=r"\Omega_\mathrm{c}h^2",
+    tau=r"\tau_\mathrm{reio}",
+)
