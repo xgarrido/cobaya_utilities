ca65d3ed7f1e54fd959701e7f552d87a05af2248
Xavier Garrido <xavier.garrido@gmail.com>
2023-09-26 09:35:06 +0200

    Update metadata

--- setup.py
+++ setup.py
@@ -24,6 +24,8 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: Scientific/Engineering :: Visualization",
         "Topic :: Education",
     ],
